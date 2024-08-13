184d10cba6bf5e9ec6930ca4e23b7a7fa7aed4c3
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2024-08-13 22:10:52 +1000

    add logo in nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import './NavBar.css';
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 
@@ -8,20 +9,26 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      &nbsp;&nbsp;
-      {user ?
-        <>
-          <Link to="" onClick={handleLogOut}>Log Out</Link>
-          &nbsp; | &nbsp;
-          <Link to="/orders">Orders</Link>
-          &nbsp; | &nbsp;
-          <Link to="/cart">Cart</Link>
-        </>
-        :
-        <Link to="/login">Log In</Link>
-      }
-    </nav>
+    <div className="header">
+      <span className="logo">
+        <span>B&nbsp;&nbsp;</span>
+        <span>&nbsp;&nbsp;H</span>
+      </span>
+      <nav>
+        <Link to="/">Home</Link>
+        &nbsp; | &nbsp;
+        {user ?
+          <>
+            <Link to="" onClick={handleLogOut}>Log Out</Link>
+            &nbsp; | &nbsp;
+            <Link to="/orders">Orders</Link>
+            &nbsp; | &nbsp;
+            <Link to="/cart">Cart</Link>
+          </>
+          :
+          <Link to="/login">Log In</Link>
+        }
+      </nav>
+    </div>
   );
 }
